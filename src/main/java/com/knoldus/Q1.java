97da5336863850111c18ee01731007f4759c3d6b
Harsh Vardhan <harsh.vardhan@knoldus.com>
2023-01-09 14:53:09 +0530

    Q1 added.

---
+++ src/main/java/com/knoldus/Q1.java
@@ -0,0 +1,26 @@
+package com.knoldus;
+
+import java.util.Scanner;
+
+/**
+ * Print your name times, without using loop;
+ */
+public class Q1 {
+
+    /*
+    The base case will be, if N becomes equal to zero, then  we will  return.
+    otherwise we'll print the name, and again call the printName function.
+     */
+    void printName(int N){
+        if(N == 0) return; // base case.
+        System.out.println("Harsh"+" ");
+        printName(N-1);
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        Q1 q1 = new Q1();
+        q1.printName(n);
+    }
+}
