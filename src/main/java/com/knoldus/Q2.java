fa42f36e17e05207038b247aa413353a6499cdd6
Harsh Vardhan <harsh.vardhan@knoldus.com>
2023-01-11 16:36:07 +0530

    added question 3

--- src/main/java/com/knoldus/Q2.java
+++ src/main/java/com/knoldus/Q2.java
@@ -4,6 +4,24 @@ import java.util.Scanner;
 
 public class Q2 {
 
+    /**
+     * The base case will be, if N becomes equal to zero, then  we will  return.
+     *  otherwise we'll print the name, and again call the solve function.
+     *
+     *     <pre>
+     *                  solve(N) --> suppose N is 3,
+     *                     |
+     *                  solve(3) --> we'll check if 3 is equal to 0 or not, base condition fails, we calls solve fun again and reduce n by 1 and print the number.
+     *                  /
+     *                solve(2) --> again check if 2 is equal to 0 or not, base condition fails, we calls solve fun again and reduce n by 1 and print the number.
+     *                /
+     *            solve(1) --> again check if  1 is equal to 0 or not, base condition fails, we calls solve fun again and reduce n by 1 and print the number.
+     *              /
+     *          solve(0)  --> now N is equal to 0,this will hit the base condition, { if(N==0) } and will return
+     *
+     *     </pre>
+     * @param n
+     */
     void solve(int n){
         if(n==0)
             return;
---
+++ src/main/java/com/knoldus/Q3.java
@@ -0,0 +1,36 @@
+package com.knoldus;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Q3 {
+
+    public static void solve(int ind, ArrayList<Integer> list, int ar[], int n){
+        if(ind == n){
+            if (list.size() > 0)
+                System.out.print(list);
+
+            if(list.size() ==0 )
+                System.out.print("{ }");
+            System.out.println();
+            return ;
+        }
+
+        solve(ind+1, list, ar, n);
+        list.add(ar[ind]);
+        solve(ind+1, list, ar, n);
+        list.remove(list.size()-1);
+
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter no of array elements: ");
+        int n = scanner.nextInt();
+        int [] ar = new int[n];
+        for (int i =0;i<n;i++){
+            ar[i] = scanner.nextInt();
+        }
+        ArrayList<Integer> list = new ArrayList<>();
+        solve(0, list, ar, n);
+    }
+}
